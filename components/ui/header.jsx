2e7bce61edd4f3a1d2367a75d8b1a9bab3869f8f
Yashpreet Singh <yashpreet1686@gmail.com>
2025-05-19 22:40:44 +0530

    completed auth with clerk

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './button'
+import { LayoutDashboard, PenBox } from 'lucide-react'
 
 const Header = () => {
   return (
@@ -13,14 +14,37 @@ const Header = () => {
           <Image src='/logo.png' className='h-12 w-auto object-contain' alt='logo' width={200} height={60} />
         </Link>
 
-        <SignedOut>
-          <SignInButton forceRedirectUrl='/dashboard'>
-            <Button variant='outline'>Login</Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <div className='flex items-center space-x-4'>
+
+          <SignedIn>
+            <Link href={'/dashboard'} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+              <Button variant='outline'>
+                <LayoutDashboard size={18} />
+                <span className='hidden md:inline'>Dashboard</span>
+              </Button>
+            </Link>
+            <Link href={'/transaction/create'}>
+              <Button className='flex items-center gap-2'>
+                <PenBox size={18} />
+                <span className='hidden md:inline'>Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/dashboard'>
+              <Button variant='outline'>Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton appearance={{
+              elements: {
+                avatarBox: '!h-10 !w-10'
+              }
+            }} />
+          </SignedIn>
+        </div>
+
       </nav>
     </div>
   )
